e8935cff651b739908ed7ea58a459b423dd4734c
kek-Sec <georgepetrakis99@gmail.com>
2020-11-17 14:17:11 +0200

    dev

---
+++ src/app/scripts/test.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class test{
+     IP:string = "http://51.91.103.150:4000/otm";
+     POST_IP:string = "http://51.91.103.150:4000/otm/add";
+} 
